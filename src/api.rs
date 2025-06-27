06b8e0679063643d091c9a8a78cc471472a09a72
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2025-06-27 01:39:37 +0000

    refactor: Simplify request handling in perform API endpoint

--- src/api.rs
+++ src/api.rs
@@ -1,7 +1,7 @@
+use actix_web::Responder;
 use actix_web::post;
 use actix_web::web;
 use actix_web::web::Data;
-use actix_web::Responder;
 use qlib_rs::Context;
 use web::Json;
 
@@ -14,17 +14,38 @@ use crate::store::Response;
  */
 #[post("/api/perform")]
 pub async fn perform(app: Data<App>, req: Json<Request>) -> actix_web::Result<impl Responder> {
-    let req = req.0.request;
-
     if req
+        .0
+        .request
         .iter()
-        .all(|r| matches!(r, qlib_rs::Request::Read{..}) )
-        {
-        let state_machine = app.state_machine_store.state_machine.read().await;
-        let value = state_machine.data.perform(&Context{}, &mut req)?;
-        return Ok(Json())
+        .all(|r| matches!(r, qlib_rs::Request::Read { .. }))
+    {
+        let ret = app.raft.ensure_linearizable().await;
+        return match ret {
+            Ok(_) => {
+                let mut store_req = req.0.request;
+                let mut state_machine = app.state_machine_store.state_machine.write().await;
+                state_machine.data.perform(&Context {}, &mut store_req)?;
+
+                Ok(Json(Response {
+                    response: store_req,
+                    error: None,
+                }))
+            }
+            Err(e) => {
+                Ok(Json(Response {
+                    response: vec![],
+                    error: Some(e.to_string()),
+                }))
+            }
+        }
     }
 
-    let response = app.raft.client_write(req).await;
-    return Ok(response)
+    match app.raft.client_write(req.0).await {
+        Ok(response) => Ok(Json(response.response().clone())),
+        Err(err) => Ok(Json(Response {
+            response: vec![],
+            error: Some(err.to_string()),
+        })),
+    }
 }
