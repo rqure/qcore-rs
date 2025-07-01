c3baef803a9743664a9c414e4b2cdf3ca0931acb
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2025-07-01 17:24:26 +0000

    feat: Remove Clone trait from StateMachineData struct

--- src/store.rs
+++ src/store.rs
@@ -681,7 +681,7 @@ pub struct StoredSnapshot {
 /// Note that we are using `serde` to serialize the
 /// `data`, which has a implementation to be serialized. Note that for this test we set both the key
 /// and value as String, but you could set any type of value that has the serialization impl.
-#[derive(Serialize, Deserialize, Debug, Default, Clone)]
+#[derive(Serialize, Deserialize, Debug, Default)]
 pub struct StateMachineData {
     pub last_applied_log: Option<LogId<NodeId>>,
 
